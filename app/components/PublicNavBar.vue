c5816a668b968a072d69af36db9ec02ec6c9f578
Kowenjko <ortoswt@gmail.com>
2025-08-29 10:17:02 +0300

    Refactor code structure for improved readability and maintainability

--- app/components/PublicNavBar.vue
+++ app/components/PublicNavBar.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '../../components/ui/button'
+</script>
 
 <template>
 	<nav class="flex justify-between items-center fixed z-50 w-full h-28 bg-gray-300 px-10 gap-4 shadow-2xl">
@@ -9,18 +11,18 @@
 		<section class="sticky top-0 flex justify-between">
 			<div class="flex flex-1 max-sm:gap-0 sm:gap-6">
 				<SignInButton>
-					<button
+					<Button
 						class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl"
 					>
 						Login
-					</button>
+					</Button>
 				</SignInButton>
 				<SignUpButton>
-					<button
+					<Button
 						class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl"
 					>
 						Register
-					</button>
+					</Button>
 				</SignUpButton>
 			</div>
 		</section>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,38 @@
 
 import { dark, neobrutalism } from '@clerk/themes'
 
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
+	css: ['~/assets/css/main.css'],
+
 	modules: [
 		'@nuxt/image',
-		'@nuxtjs/tailwindcss',
 		// '@prisma/nuxt',
 		'@clerk/nuxt',
+		'shadcn-nuxt',
+		'@vueuse/nuxt',
 	],
 	clerk: {
 		appearance: {
 			baseTheme: neobrutalism,
 		},
+	},
+
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: './components/ui',
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 })
